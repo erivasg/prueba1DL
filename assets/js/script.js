036505cae844254cf4176c041e73726e477ce346
Esteban Rivas Gaete <erivas@MacBookPro.local>
2020-01-30 12:46:57 -0300

    Tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,16 @@
-$(function(){
+$(function() {
 
     $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-    }, 800, function(){
-
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-
+    $('[data-toggle="tooltip"]').tooltip();
 });
